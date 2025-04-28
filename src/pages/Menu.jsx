1630944c66bd2a898f4192df2824ea512ee462dd
sarameseha <ssm4p@mtmail.mtsu.edu>
2025-04-28 13:28:19 -0500

    Update Menu.jsx

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,17 +1,17 @@
+// src/pages/Menu.jsx
 import React from "react";
+import "../styles/Menu.css";
 import menu1 from "../assets/Menu 1.jpg";
 import menu2 from "../assets/Menu 2.jpg";
 
 const Menu = () => {
   return (
-    <div>
-      <h2>Our Menu</h2>
-      <img
-        src={menu1}
-        alt="Menu 1"
-        style={{ width: "100%", marginBottom: "20px" }}
-      />
-      <img src={menu2} alt="Menu 2" style={{ width: "100" }} />
+    <div className="menu-container">
+      <h2 className="menu-title">Our Menu</h2>
+      <div className="menu-images">
+        <img src={menu1} alt="Menu 1" className="menu-img" />
+        <img src={menu2} alt="Menu 2" className="menu-img" />
+      </div>
     </div>
   );
 };
